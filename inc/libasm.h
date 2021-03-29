171f9e5914a62d07270183ec4bf5aa13676c594a
hyungraelee <hyunlee@student.42seoul.kr>
2021-03-30 00:10:06 +0900

    making ft_strlen

--- inc/libasm.h
+++ inc/libasm.h
@@ -6,13 +6,22 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 22:13:38 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/03/29 22:23:03 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/03/30 00:04:12 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
 
-int hello(void);
+# include <stdlib.h>
+
+// int hello(void);
+size_t	ft_strlen(const char *s);
+// char *	ft_strcpy(char * dst, const char * src);
+// int		ft_strcmp(const char *s1, const char *s2);
+// ssize_t	ft_write(int fd, const void *buf, size_t nbyte);
+// ssize_t	ft_read(int fd, void *buf, size_t nbyte);
+// char *	ft_strdup(const char *s1);
+
 
 #endif
---
+++ srcs/ft_strlen.s
@@ -0,0 +1,28 @@
+section .text
+	global _ft_strlen
+
+_ft_strlen:
+	mov	rax, 0
+	jmp	loop
+
+loop:
+	cmp	rdi, 0
+	je	end
+	inc	rax
+	inc rdi
+	jmp loop
+
+end:
+	ret
+
+
+
+; size_t	ft_strlen(const char *s)
+; {
+; 	size_t	res;
+
+; 	res = 0;
+; 	while (s++)
+; 		res++;
+; 	return (res);
+; }
