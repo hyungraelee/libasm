f8c9036514228bf5b37b11279cf6cf84d923f1e9
hyungraelee <hyunlee@student.42seoul.kr>
2021-04-03 02:14:54 +0900

    done mandatory test code

--- inc/libasm.h
+++ inc/libasm.h
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 22:13:38 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/04/01 20:28:55 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/04/03 02:04:35 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@
 # include <string.h>
 # include <errno.h>
 
+# define C_LIME "\e[1;38;2;133;188;94m"
+# define C_PURPLE "\e[1;38;2;189;147;249m"
+# define C_LEMON "\e[1;38;2;235;247;165m"
+# define C_P "\e[1;38;2;255;192;91m"
+# define C_O "\e[1;38;2;75;221;241m"
+# define C_I "\e[1;38;2;244;66;236m"
 # define C_YLW "\e[0;33m"
 # define C_RED "\033[31m"
 # define B_RED "\033[1;5;31m"
@@ -30,9 +36,8 @@
 size_t	ft_strlen(const char *s);
 char *	ft_strcpy(char * dst, const char * src);
 int		ft_strcmp(const char *s1, const char *s2);
+char *	ft_strdup(const char *s1);
 ssize_t	ft_write(int fd, const void *buf, size_t nbyte);
 ssize_t	ft_read(int fd, void *buf, size_t nbyte);
-char *	ft_strdup(const char *s1);
-
 
 #endif
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 22:21:33 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/04/01 22:10:05 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/04/03 02:12:14 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,69 +15,103 @@
 
 void	check_ft_strlen(void)
 {
-	printf("%s", B_GREEN);
+	printf("%s", C_LIME);
+	printf("\nCheck >>> FT_STRLEN\n");
 	printf("-------------------------------------\n");
-	printf("Check >>> ft_strlen\n");
 	printf("result of 'empty string'	= %zu\n", ft_strlen(""));
 	printf("result of 'hello'		= %zu\n", ft_strlen("hello"));
 	printf("result of 'long string'		= %zu\n", \
-	ft_strlen("12345678901234567890abcdefghijklmnopqrstuvwxyz12345678901234567890"));
+	ft_strlen("IlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogi"));
 	printf("-------------------------------------\n\n");
-	printf("%s", C_RESET);
 }
 
 void	check_ft_strcpy(void)
 {
-	// char	dst[] = "abcde";
 	char	dst[200];
 
-	printf("%s", B_YLW);
+	printf("%s", C_PURPLE);
+	printf("Check >>> FT_STRCPY\n");
 	printf("-------------------------------------\n");
-	printf("Check >>> ft_strcpy\n");
-	// printf("dst : %s\n", dst);
 	printf("result of copy 'empty string'	= %s\n", ft_strcpy(dst, ""));
 	printf("result of copy 'ABC'		= %s\n", ft_strcpy(dst, "ABC"));
-	printf("result of copy 'long string'	= %s\n", ft_strcpy(dst, "12345678901234567890abcdefghijklmnopqrstuvwxyz12345678901234567890"));
-	// printf("result of copy 'long string'	= %s\n", ft_strcpy(dst, "12345678"));
+	printf("result of copy 'long string'	= %s\n", ft_strcpy(dst, "IlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogi"));
 	printf("-------------------------------------\n\n");
-	printf("%s", C_RESET);
 }
 
-// void	check_ft_strcmp(void)
-// {
-// 	printf("-------------------------------------\n");
-// 	printf("%d\n", ft_strcmp("c", "a"));
-// 	printf("%d\n", ft_strcmp("abc", "abcd"));
-// 	printf("-------------------------------------\n");
-// }
+void	check_ft_strcmp(void)
+{
+	printf("%s", C_LEMON);
+	printf("Check >>> FT_STRCMP\n");
+	printf("-------------------------------------\n");
+	printf("result of '2 empty strings'			= %d\n", ft_strcmp("", ""));
+	printf("result of '1 empty string as first argument'	= %d\n", ft_strcmp("", "a"));
+	printf("result of '1 empty string as second argument'	= %d\n", ft_strcmp("a", ""));
+	printf("result of \"abc\" and \"abc\"			= %d\n", ft_strcmp("abc", "abc"));
+	printf("result of \"abc\" and \"abcd\"			= %d\n", ft_strcmp("abc", "abcd"));
+	printf("result of \"abcd\" and \"abc\"			= %d\n", ft_strcmp("abcd", "abc"));
+	printf("-------------------------------------\n\n");
+}
 
-void	test_write_read(void)
+void	check_ft_strdup(void)
+{
+	char	*str;
+
+	printf("%s", C_I);
+	printf("Check >>> FT_STRDUP\n");
+	printf("-------------------------------------\n");
+	str = ft_strdup("");
+	printf("result of 'empty string'		= %s\n", str);
+	str = ft_strdup("Do you like gogi?");
+	printf("result of 'Do you like gogi?'		= %s\n", str);
+	str = ft_strdup("IlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogiIlikegogi");
+	printf("result of 'long string'			= %s\n", str);
+	printf("-------------------------------------\n\n");
+
+}
+
+void	check_ft_write(void)
 {
 	int		fd;
-	char	buf[10];
 
-	printf("\n\n*ft_write test\n");
 	fd = open("./test.txt", O_WRONLY);
-	printf("\nwrite return : %zd\n", ft_write(1, "hello", 5));
-	printf("\nwrite return : %zd\n", ft_write(-1, "hello", 5));
-	printf("\nwrite return : %zd\n", ft_write(fd, "gogijoa", 7));
+	printf("%s", C_P);
+	printf("Check >>> FT_WRITE\n");
+	printf("-------------------------------------\n");
+	printf("\nresult of 'standard out' with \"gogijoa\" 	= %zd\n", ft_write(1, "gogijoa", 7));
+	printf("result of 'user fd' with 'test.txt' 		= %zd\n", ft_write(fd, "gogijoa", 7));
+	printf("\ncompare errno with 'fd = -1'\n");
+	printf("result of write		= %zd, errno = %d\n", write(-1, "hello", 5), errno);
+	printf("result of ft_write	= %zd, errno = %d\n", ft_write(-1, "hello", 5), errno);
+	printf("-------------------------------------\n\n");
 	close(fd);
-	printf("\n\n*ft_read test\n");
+}
+
+void	check_ft_read(void)
+{
+	int		fd;
+	char	buf[30];
+
 	fd = open("./test.txt", O_RDONLY);
-	printf("read return : %zd\n", ft_read(0, buf, 5));
-	printf("read buf : %s\n\n", buf);
-	printf("read return : %zd\n", ft_read(fd, buf, 8));
-	printf("read buf : %s\n\n", buf);
-	printf("read return : %zd\n", ft_read(-1, buf, 5));
+	printf("%s", C_O);
+	printf("Check >>> FT_READ\n");
+	printf("-------------------------------------\n");
+	printf("Please input string!\n");
+	printf("result of 'standard in'			= %zd\n", ft_read(0, buf, 10));
+	printf("READ BUF : %s", buf);
+	printf("result of 'user fd' with 'test.txt'	= %zd\n", ft_read(fd, buf, 10));
+	printf("READ BUF : %s\n", buf);
+	printf("\ncompare errno with 'fd = -1'\n");
+	printf("result of read		= %zd, 	errno = %d\n", read(-1, buf, 10), errno);
+	printf("result of ft_read	= %zd, 	errno = %d\n", ft_read(-1, buf, 10), errno);
+	printf("-------------------------------------\n\n");
 }
 
 int main()
 {
 	check_ft_strlen();
 	check_ft_strcpy();
-	test_write_read();
-	// check_ft_strcmp();
-	// check_ft_write();
+	check_ft_strcmp();
+	check_ft_strdup();
+	check_ft_write();
 	// check_ft_read();
-	// check_ft_strdup();
 }
